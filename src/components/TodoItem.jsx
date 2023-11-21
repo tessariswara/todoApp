35195629adeba0b76c3a74c95822251c1df4bb8a
eca <tessa.projectmast@outlook.com>
2023-11-21 16:39:23 +0700

    Add CSS to components

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const TodoItem = (({ todo }) => {
-    return <p>{todo.title}</p>
-})
+const TodoItem = ({ todo }) => {
+    return (
+        <div style={ styles }>
+            <p>{todo.title}</p>
+        </div>
+    );
+}
+
+const styles = {
+    todoItem: {
+      border: '2px solid #f4f4f4',
+      fontSize: '24px',
+    },
+  }
 
 export default TodoItem
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -3,12 +3,19 @@ import TodoItem from './TodoItem'
 
 const Todos = ({ todos }) => {
     return (
-        <div>
+        <div style={ styles }>
             {todos.map((todo) => {
                 return <TodoItem key={todo.id} todo={todo}/>
             })}
         </div>
     )
 }
+
+const styles = {
+    container: {
+        width: '40%',
+        margin: '0 auto',
+    },
+}  
 
 export default Todos
